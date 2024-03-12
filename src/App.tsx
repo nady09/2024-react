29d9058bea7948e672b93d4a1b4b8d8e086ff18e
nadynne <105674772+nady09@users.noreply.github.com>
2024-03-12 17:18:05 -0300

    aula 12/03

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+      <div>
+        <h1>Bem vindo ao mundo React</h1>
+        <button>eu sou um botão html</button>
+      </div>
+    );
+  }
+  
+  export default App;
